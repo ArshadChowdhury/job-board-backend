8951dbd9c777495a7ef111813692c27524316e14
Mohammed Arshad <arshadchowdhury46@gmail.com>
2025-07-24 18:06:04 +0600

    Deploying the project to vercel so I can access it from frontend

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,13 +4,15 @@ import { Application } from '../modules/applications/entities/application.entity
 
 export const databaseConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
+  // host: process.env.DATABASE_HOST || 'localhost',
+  url: process.env.DATABASE_URL,
   port: parseInt(process.env.DATABASE_PORT ?? '5432') || 5432,
+  autoLoadEntities: true,
   username: process.env.DATABASE_USERNAME || 'postgres',
   password: process.env.DATABASE_PASSWORD || 'password',
   database: process.env.DATABASE_NAME || 'job-board',
   entities: [Job, Application],
-  synchronize: process.env.NODE_ENV === 'development', // Only in development
+  synchronize: process.env.NODE_ENV === 'production', // Only in development
   logging: process.env.NODE_ENV === 'development',
   ssl:
     process.env.NODE_ENV === 'production'
